6b2444e603f8630d1b2d15de0da26ea5e7e8c980
AlbertUW807 <50271083+AlbertUW807@users.noreply.github.com>
2020-01-19 00:23:54 -0500

    Add files via upload
    React Build

---
+++ movie-prediction/build/precache-manifest.7492b67aba57bc1f127560246b6db016.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3049b7a136597e1698148969b5b8e17d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bcf6f4e72a92e501cc03",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "edf9554212bb5845d5dc",
+    "url": "/static/js/2.6146e200.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.6146e200.chunk.js.LICENSE"
+  },
+  {
+    "revision": "bcf6f4e72a92e501cc03",
+    "url": "/static/js/main.b92a7f7d.chunk.js"
+  },
+  {
+    "revision": "c8c17f9ce13b7eea8d11",
+    "url": "/static/js/runtime-main.badfe7f3.js"
+  }
+]);
